cfb124f2f4d9dec69dcefc4c6a624d24fa2e8312
AlexAkmaev <permafried2000@gmail.com>
2022-05-31 13:39:35 +0300

    Correct global funcs

--- llvm/lib/Target/Sim/Sim.h
+++ llvm/lib/Target/Sim/Sim.h
@@ -17,35 +17,27 @@
 
 #include "MCTargetDesc/SimMCTargetDesc.h"
 #include "llvm/Target/TargetMachine.h"
-#include "llvm/CodeGen/MachineInstr.h"
-#include "llvm/CodeGen/AsmPrinter.h"
 #include "llvm/Pass.h"
 
 namespace llvm {
 class SimTargetMachine;
+class MachineInstr;
+class FunctionPass;
+class AsmPrinter;
 
-bool lowerSimMachineInstrToMCInst(const MachineInstr *MI, MCInst &OutMI,
-                                  AsmPrinter &AP);
-bool lowerSimMachineOperandToMCOperand(const MachineOperand &MO,
-                                       MCOperand &MCOp, const AsmPrinter &AP);
+bool lowerSimMachineInstrToMCInst(const MachineInstr *MI, MCInst &OutMI, AsmPrinter &AP);
 
-FunctionPass *createSimISelDag(SimTargetMachine &TM,
-                               CodeGenOpt::Level OptLevel);
+bool lowerSimMachineOperandToMCOperand(const MachineOperand &MO, MCOperand &MCOp, const AsmPrinter &AP);
+
+FunctionPass *createSimISelDag(SimTargetMachine &TM, CodeGenOpt::Level OptLevel);
 
 namespace SIM {
 enum {
-  // changed for compatibility with emulator
   GP = SIM::R0,
   RA = SIM::R1,
   SP = SIM::R2,
   FP = SIM::R3,
   BP = SIM::R4,
-  // Similar to RISC-V
-  // RA = SIM::R0,
-  // SP = SIM::R1,
-  // GP = SIM::R2,
-  // FP = SIM::R3,
-  // BP = SIM::R4,
 };
 } // end namespace SIM;
 
